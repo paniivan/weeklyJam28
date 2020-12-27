246bd71634fa385f8857c6e60cc992bcc7fb1237
alexanderguk <alex.guk.kpi@gmail.com>
2020-12-28 00:40:07 +0400

    Some basic project struture and movement test

---
+++ Assets/Scripts/RouteFollow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class RouteFollow : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject m_routeFrom;
+    public GameObject m_routeTo;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        const float speed = 1.0f;
+
+        if (!m_routeFrom || !m_routeTo)
+        {
+            return;
+        }
+
+        Vector3 dir = (m_routeTo.transform.position - m_routeFrom.transform.position).normalized;
+        Vector3 velocity = speed * dir;
+
+        transform.position = transform.position + velocity * Time.deltaTime;
+    }
+}
